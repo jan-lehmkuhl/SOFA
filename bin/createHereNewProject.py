a43dc2722b233836a57b13912ade5fa091b41ddd
jan <janlehmkuhl@web.de>
2019-09-26 22:37:38 +0200

    [core] #ADD git diff for zipped files

--- bin/createHereNewProject.py
+++ bin/createHereNewProject.py
@@ -4,28 +4,35 @@ import os
 import shutil
 
 
-#################################################
 # init repository
-#################################################
+# =================================================================================================
 
 os.system("git init")
 os.system("git submodule add git@gitlab.com:schlupp/simulation-projects.git tools/framework ")
 
 
-#################################################
 # copy first files
-#################################################
-
+# =================================================================================================
 dirpath = os.getcwd()
 # print("current directory is : " + dirpath)
+
+# copy dummy files
+# -----------------------------------------------------------------------------
 shutil.copyfile( dirpath +"/tools/framework/root-dummies/Makefile"      , dirpath +"/Makefile" )
 shutil.copyfile( dirpath +"/tools/framework/root-dummies/project.json"  , dirpath +"/project.json" )
 shutil.copyfile( dirpath +"/tools/framework/root-dummies/.gitignore"    , dirpath +"/.gitignore" )
+shutil.copyfile( dirpath +"/tools/framework/root-dummies/.gitattributes", dirpath +"/.gitattributes" )
+
+# alter ./.git/config
+# -----------------------------------------------------------------------------
+configfile=open("./.git/config", "a+")
+appendfile=open("./tools/framework/root-dummies/gitconfig-addition", "r")
+appendstring=appendfile.read()
+configfile.write("\n"+appendstring)
 
 
-#################################################
 # first git commit
-#################################################
+# =================================================================================================
 
 os.system("git add Makefile")
 os.system("git add project.json")
