78c92c452a8ab1f080c5b7506c64fcbf0e0173c3
jan <janlehmkuhl@web.de>
2019-10-05 21:16:05 +0200

    [freecad] #ADD workflow for run

--- openFoam/dummies/makefiles/Makefile_case_run.mk
+++ openFoam/dummies/makefiles/Makefile_case_run.mk
@@ -1,5 +1,15 @@
 # Makefile copied from ./tools/framework/openFoam/dummies/makefiles/Makefile_run.mk
 
+
+# include ../../../tools/framework/global-make.mk
+
+meshFolder       = $(shell node -p "require('./run.json').buildSettings.meshLink")
+freecadFolder    = $(shell node -p "require('../../mesh/$(meshFolder)/mesh.json').buildSettings.freecadLink")
+
+
+# handle framework related run folder
+# =============================================================================
+
 # initialize case according to run.json
 init:
 	python3 ../../../tools/framework/openFoam/python/openFoam.py initCase
@@ -20,6 +30,10 @@ clear:
 commit:
 	python3 ../../../tools/framework/openFoam/python/openFoam.py commit
 
+
+# run
+# =============================================================================
+
 # run case according to run.json
 run:
 	python3 ../../../tools/framework/openFoam/python/foamRun.py run
@@ -30,5 +44,29 @@ cleanRun:
 
 # open paraview
 view:
-	python3 ../../../tools/framework/openFoam/python/foamRun.py view
+	if [ ! -f "Allrun" ] ; then                                                 \
+		echo "*** start foamMesh.py" ;                                          \
+		python3 ../../../tools/framework/openFoam/python/foamRun.py view ;      \
+	elif [ -f "pv.foam" ] ; then                                                \
+		echo "*** start paraFoam -builtin" ;                                     \
+		paraFoam -builtin ;                                                      \
+	fi ;
+
+
+
+# FreeCAD settings
+# =============================================================================
+
+linkfreecad:
+	if [ -d cad ] ; then   rm cad   ; fi ;
+	ln -s   ../../cad/$(freecadFolder)  cad
+	if [ -d mesh ] ; then   rm mesh   ; fi ;
+	ln -s   ../../mesh/$(meshFolder)  mesh
+
+
+# can be used to overwrite the dummy settings
+copyfreecadcasefiles: linkfreecad
+	cp -rf ../../cad/$(freecadFolder)/case/* .
+	sed -i 's\MESHDIR="../meshCase"\MESHDIR="./mesh"\' Allrun
+
 
