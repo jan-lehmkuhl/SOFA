530d5a972e91ac8afd7d22a1397a17d5499bd9c6
Jan Lehmkuhl <janlehmkuhl@web.de>
2019-10-05 11:20:13 +0200

    [mesh] #CHANGE indents and sorting

--- openFoam/dummies/makefiles/Makefile_case_cad.mk
+++ openFoam/dummies/makefiles/Makefile_case_cad.mk
@@ -1,7 +1,7 @@
 # Makefile copied from ./tools/framework/openFoam/dummies/makefiles/Makefile_cad.mk
 
 
-# framework handling
+# framework folder handling
 # =============================================================================
 
 # clone case to a new case with the next available running number 
@@ -19,14 +19,17 @@ commit:
 	python3 ../../../tools/framework/openFoam/python/openFoam.py commit
 
 
-# check topology of stl files and write log file
-checkSurfaces:
-	python3 ../../../tools/framework/openFoam/python/foamCad.py checkSurfaces
-
-
 # erase all vtk files
 cleanVTK:
 	python3 ../../../tools/framework/openFoam/python/foamCad.py cleanVTK
+
+
+# Basic stl/surface handling
+# =============================================================================
+
+# check topology of stl files and write log file
+checkSurfaces:
+	python3 ../../../tools/framework/openFoam/python/foamCad.py checkSurfaces
 
 
 # combine all stl files into a single regional stl
@@ -34,11 +37,10 @@ combineSTL:
 	python3 ../../../tools/framework/openFoam/python/foamCad.py combineSTL
 
 
-# GUI handling
+# FreeCAD handling
 # =============================================================================
 
-# opencad
-openfreecad:
+openfreecadgui:
 	if [ ! -f native/geometry.FCStd ]; then cp ../../../tools/framework/openFoam/dummies/cad/geometry.FCStd  native/geometry.FCStd; fi
 	freecad native/geometry.FCStd
 
@@ -47,6 +49,9 @@ openfreecad:
 difffreecad:
 	git diff  --color-words  native/geometry.FCStd
 
+
+# Paraview
+# =============================================================================
 
 # open paraview
 view:
--- openFoam/dummies/makefiles/Makefile_case_mesh.mk
+++ openFoam/dummies/makefiles/Makefile_case_mesh.mk
@@ -1,24 +1,33 @@
 # Makefile copied from ./tools/framework/openFoam/dummies/makefiles/Makefile_mesh.mk
 
-# initialize case according to mesh.json
-init:
-	python3 ../../../tools/framework/openFoam/python/openFoam.py initCase
 
-# renew the symlinks to cad 
-updateSymlinks:
-	python3 ../../../tools/framework/openFoam/python/openFoam.py symlinks
+# handle framework related mesh folder
+# =============================================================================
 
 # clone case to a new case with the next available running number 
 clone:
 	python3 ../../../tools/framework/openFoam/python/openFoam.py clone
 
-# erase all files except necessary
+# erase all files except framework necessary files
 clear:
 	python3 ../../../tools/framework/openFoam/python/openFoam.py clear
 
 # commit all changes inside case
 commit:
 	python3 ../../../tools/framework/openFoam/python/openFoam.py commit
+
+
+# full-control OpenFOAM meshing operations
+# =============================================================================
+
+# initialize case according to mesh.json
+initOpenFoamMesh:
+	python3 ../../../tools/framework/openFoam/python/openFoam.py initCase
+
+
+# renew the symlinks to cad 
+updateSymlinks:
+	python3 ../../../tools/framework/openFoam/python/openFoam.py symlinks
 
 # generate mesh according to mesh.json
 mesh:
@@ -35,6 +44,11 @@ finalizeMesh:
 # erase all meshing results
 cleanMesh:
 	python3 ../../../tools/framework/openFoam/python/foamMesh.py cleanMesh
+
+
+
+# GUI operations
+# =============================================================================
 
 # open paraview
 view:
