22541810f3750aee3d3d5bbc3c7e3739b6de327b
Jan Lehmkuhl <2695300-schlupp@users.noreply.gitlab.com>
2022-03-23 23:30:12 +0100

    [tests/logs] FIX variable time 001 s

--- makefile.global.mk
+++ makefile.global.mk
@@ -41,4 +41,5 @@ remove_full_path_with_sed = sed --in-place --regexp-extended --expression \
 	s/( = )[0-9\.]* s/\1x.xx s/g; \
 	s/(Case   : )(\/.*)(\/sofa-framework.*)/\1\3/g; \
 	s/(Slaves : ).*/\1xxx/g; \
+	s/(.* in )([0-9][0-9\.]*) s/\1xxx s/g; \
 	s/(.*\/tmp)(.*)(\.html.*)/\1xxxx\3/g"
