86b08ddc3e1fec7326e6986322d7881e814191ce
Jan Lehmkuhl <2695300-schlupp@users.noreply.gitlab.com>
2022-03-24 09:44:53 +0100

    [tests/logs] FIX variing size after .

--- makefile.global.mk
+++ makefile.global.mk
@@ -29,7 +29,9 @@ list_content = ls --almost-all -g --no-group \
     --time-style='+' --human-readable --group-directories-first --classify --recursive  \
     | sed -re 's/^[^ ]* //'   \
     | sed -e 's/,/./g'  \
-    | sed -re 's/([1-9] )( [ 1-9][0-9]{0,2})(  [a-zA-Z]*)/\1tiny\3/g'
+    | sed -re 's/([1-9] )( [ 1-9][0-9]{0,2})(  [a-zA-Z]*)/\1tiny\3/g' \
+    | sed -re 's/([1-9] [ 1-9]\.)([0-9])([KM]  .*)/\1x\3/g'
+
 remove_full_path_with_sed = sed --in-place --regexp-extended --expression \
     "s/(make.*'\/)(.*)(tests.*)/\1\3/g; \
     s/([0-9]:[0-9]{2}:[0-9]{2}.[0-9]{6})/x:xx:xx.xxxxxx/g; \
@@ -37,7 +39,7 @@ remove_full_path_with_sed = sed --in-place --regexp-extended --expression \
 	s/(on )(.*)( using)/\1LocalMachine\3/g; \
 	s/(Date   : )(.*) [0-9]{2} [0-9]{4}/\1xxx xx xxxx/g; \
 	s/(Host   : )\".*\"/\1\"LocalMachine\"/g; \
-	s/(PID    : )[0-9]{7}/\1xxxxxx/g; \
+	s/(PID    : )[0-9]{4,7}/\1xxxxxx/g; \
 	s/( = )[0-9\.]* s/\1x.xx s/g; \
 	s/(Case   : )(\/.*)(\/sofa-framework.*)/\1\3/g; \
 	s/(Slaves : ).*/\1xxx/g; \
