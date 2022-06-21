b30b2a44b22ac3bd61ca7ffdc3f692024b07b934
Jan Lehmkuhl <2695300-schlupp@users.noreply.gitlab.com>
2022-06-21 10:34:44 +0200

    [tests] IMPROVE list content replacement

--- makefile.global.mk
+++ makefile.global.mk
@@ -43,9 +43,17 @@ list_content = ls --almost-all -g --no-group \
     --time-style='+' --human-readable --group-directories-first --classify --recursive  \
     | sed -re 's/^[^ ]* //'   \
     | sed -e 's/,/./g'  \
-    | sed -re 's/([1-9] )( [ 1-9][0-9]{0,2})(  [a-zA-Z]*)/\1tiny\3/g' \
+    | sed -re 's/([1-9] )( *[ 1-9][0-9]{0,2})(  [a-zA-Z]*)/\1tiny\3/g' \
     | sed -re 's/^([0-9]{0,3})(\.[0-9])*([KM])/xxx\3/g' \
     | sed -re 's/([1-9] )([ 1-9]\.[0-9])([KM]  .*)/\1x.x\3/g'
+
+# SED notes: 
+#   - remove empty double lines
+#   - replace , by .
+#   - replace plain digigs by tiny
+#   - replace folder summary
+#   - replace numbers with dots
+
 
 remove_logs_variable_content = sed --in-place --regexp-extended --expression \
     "s/(make.*'\/)(.*)(tests.*)/\1\3/g; \
